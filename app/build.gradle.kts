b6c3bb1915a018def0997cbdbb0b3c48a872e750
chaixshot <chai5142xshot@gmail.com>
2026-09-17 17:23:10 +0700

    pump version 4.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "helium314.keyboard"
         minSdk = 21
         targetSdk = 37
-        versionCode = 4101
-        versionName = "4.1"
+        versionCode = 411
+        versionName = "4.1.1 Pico"
         ndk {
             abiFilters.clear()
             abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
